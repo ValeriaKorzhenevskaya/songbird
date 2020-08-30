a7a2e6d58d5ed33c787849426f70abad8dde89f3
Valeria <Valeryia.Karzhaneuskaya@mail.ru>
2020-08-30 21:35:00 +0300

    feat: delete button in modal window, when score 30/30

--- my-app/src/Modal.js
+++ my-app/src/Modal.js
@@ -4,6 +4,9 @@ import React from 'react';
 class Modal extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      maxScore: 30,
+    }
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -16,8 +19,10 @@ class Modal extends React.Component {
       <div className={this.props.endOfTheGame ? "modal" : "modal none"}>
         <div className="wrapper">
           <h1 className="modal__title">ПОЗДРАВЛЯЕМ!</h1>
-          <h2 className="modal__text">Вы набрали {this.props.score} из 30 баллов</h2>
-          <button onClick={this.handleClick} className="button close-button">Новая игра</button>
+          <h2 className="modal__text">Вы набрали {this.props.score} из {this.state.maxScore} баллов</h2>
+          <div className="modal__button">
+            {this.props.score === this.state.maxScore ? "" : <button onClick={this.handleClick} className="button close-button">Новая игра</button>}
+          </div>
         </div>
       </div>
     )
